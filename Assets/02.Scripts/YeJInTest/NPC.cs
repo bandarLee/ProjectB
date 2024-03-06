0fc2b016c854937db2be4225aafcba00b8eecaf4
yeeejini <rkdska231027@gmail.com>
2024-03-06 19:24:16 +0900

    [민예진] 폰트 폴더 생성 / 폰트 추가

---
+++ Assets/02.Scripts/YeJInTest/NPC.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            _animator.SetTrigger("work");
+
+        }
+    }
+    
+}
