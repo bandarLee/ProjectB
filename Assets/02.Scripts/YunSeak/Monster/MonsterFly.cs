d8b333a6f20681be596b6c79b9b6da5076c73f76
DESKTOP-358CSLP\USER <dbstjr7412369@gmail.com>
2024-03-07 18:32:05 +0900

    [이윤석] 사이버 펑크 몬스터 작업 중

--- Assets/02.Scripts/YunSeak/Monster/MonsterFly.cs
+++ Assets/02.Scripts/YunSeak/Monster/MonsterFly.cs
@@ -4,28 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
 
-public enum MonsterState // 몬스터의 상태
+public enum MonsterType // 몬스터의 종류
 {
-    Idle,           // 대기
-    Patrol,         // 순찰
-    Trace,          // 추적
-    Attack,         // 공격
-    Comeback,       // 복귀
+    Follow,
+    Fire
 }
 
-public class MonsterNormal : MonoBehaviour
+public class MonsterFly : MonoBehaviour
 {
-
     [Range(0, 100)]
     public int Health;
     public int MaxHealth = 100;
     public Slider HealthSliderUI;
+    public MonsterType Type = MonsterType.Follow;
     /***********************************************************************/
 
     //private CharacterController _characterController;
     private NavMeshAgent _navMeshAgent;
     private Animator _animator;
-
 
     private Transform _target;         // 플레이어
     public float FindDistance = 5f;  // 감지 거리
@@ -48,7 +44,20 @@ public class MonsterNormal : MonoBehaviour
 
     private MonsterState _currentState = MonsterState.Idle;
 
+    /******************************************************************************/
 
+    // 목표: 폭발 범위 데미지 기능 구현
+    // 필요 속성:
+    // - 범위
+    public float ExplosionRadius = 3;
+    // 구현 순서:
+
+
+    public int Damage = 60;
+
+    public GameObject BombEffectPrefab;
+
+    private Collider[] _colliders = new Collider[10];
 
     private void Start()
     {
@@ -68,35 +77,52 @@ public class MonsterNormal : MonoBehaviour
 
     public void Init()
     {
-        _idleTimer = 0f;
+        //_idleTimer = 0f;
         Health = MaxHealth;
     }
-
-    private void Update()
+    void Update()
     {
-        //HealthSliderUI.value = (float)Health / (float)MaxHealth;  // 0 ~ 1
-
-        // 상태 패턴: 상태에 따라 행동을 다르게 하는 패턴 
-        // 1. 몬스터가 가질 수 있는 행동에 따라 상태를 나눈다.
-        // 2. 상태들이 조건에 따라 자연스럽게 전환(Transition)되게 설계한다.
-
-        switch (_currentState)
+        if (MonsterType.Follow == Type) 
         {
-            case MonsterState.Idle:
-                Idle();
-                break;
+            switch (_currentState)
+            {
+                case MonsterState.Idle:
+                    Idle();
+                    break;
 
-            case MonsterState.Patrol:
-                Patrol();
-                break;
+                case MonsterState.Patrol:
+                    Patrol();
+                    break;
 
-            case MonsterState.Trace:
-                Trace();
-                break;
+                case MonsterState.Trace:
+                    Trace();
+                    break;
 
-            case MonsterState.Comeback:
-                Comeback();
-                break;
+                case MonsterState.Comeback:
+                    Comeback();
+                    break;
+            }
+        }
+        else if (MonsterType.Fire == Type) 
+        {
+            switch (_currentState)
+            {
+                case MonsterState.Idle:
+                    Idle();
+                    break;
+
+                case MonsterState.Patrol:
+                    Patrol();
+                    break;
+
+                case MonsterState.Trace:
+                    GunFire();
+                    break;
+
+                case MonsterState.Comeback:
+                    Comeback();
+                    break;
+            }
         }
     }
 
@@ -121,7 +147,6 @@ public class MonsterNormal : MonoBehaviour
             _currentState = MonsterState.Trace;
         }
     }
-
     private void Trace()
     {
         // Trace 상태일때의 행동 코드를 작성
@@ -160,21 +185,24 @@ public class MonsterNormal : MonoBehaviour
 
     private void Patrol()
     {
-        _navMeshAgent.stoppingDistance = 0f;
-        _navMeshAgent.SetDestination(PatrolTarget.position);
-
-        if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance <= TOLERANCE)
+        
         {
-            Debug.Log("상태 전환: Patrol -> Comeback");
-            _animator.SetTrigger("PatrolToComeback");
-            _currentState = MonsterState.Comeback;
-        }
+            _navMeshAgent.stoppingDistance = 0f;
+            _navMeshAgent.SetDestination(PatrolTarget.position);
 
-        if (Vector3.Distance(_target.position, transform.position) <= FindDistance)
-        {
-            Debug.Log("상태 전환: Patrol -> Trace");
-            _animator.SetTrigger("PatrolToTrace");
-            _currentState = MonsterState.Trace;
+            if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance <= TOLERANCE)
+            {
+                Debug.Log("상태 전환: Patrol -> Comeback");
+                _animator.SetTrigger("PatrolToComeback");
+                _currentState = MonsterState.Comeback;
+            }
+
+            if (Vector3.Distance(_target.position, transform.position) <= FindDistance)
+            {
+                Debug.Log("상태 전환: Patrol -> Trace");
+                _animator.SetTrigger("PatrolToTrace");
+                _currentState = MonsterState.Trace;
+            }
         }
     }
 
@@ -211,5 +239,28 @@ public class MonsterNormal : MonoBehaviour
             _animator.SetTrigger("ComebackToIdle");
             _currentState = MonsterState.Idle;
         }
+    }
+    private void GunFire()
+    {
+
+
+
+    }
+    // 1. 터질 때
+    private void OnColliderEnter(Collider other)
+    {
+        
+        gameObject.SetActive(false); // 창고에 넣는다.
+
+        GameObject effect = Instantiate(BombEffectPrefab);
+        effect.transform.position = this.gameObject.transform.position;
+
+        // 2. 범위안에 있는 모든 콜라이더를 찾는다.
+        // -> 피직스.오버랩 함수는 특정 영역(radius) 안에 있는 특정 레이어들의 게임 오브젝트의
+        //    콜라이더 컴포넌트들을 모두 찾아 배열로 반환하는 함수
+        // 영역의 형태: 스피어, 큐브, 캡슐
+        int layer =/* LayerMask.GetMask("Player") |*/ LayerMask.GetMask("Plaer");
+        int count = Physics.OverlapSphereNonAlloc(transform.position, ExplosionRadius, _colliders, layer);
+        // 3. 찾은 콜라이더 중에서 타격 가능한(IHitable) 오브젝트를 찾아서 Hit()한다.
     }
 }
