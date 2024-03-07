e9a52b11c135888129e16fa48e2b402fef65f410
thlee <sheena1131@kakao.com>
2024-03-07 11:59:24 +0900

    [이태환] 인공위성 새로 업로드

--- Assets/02.Scripts/PlayerInput.cs
+++ Assets/02.Scripts/PlayerInput.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     public string moveAxisName = "Vertical"; // 앞뒤 움직임을 위한 입력축 이름
-    public string rotateAxisName = "Horizontal"; // 좌우 회전을 위한 입력축 이름
+    public string moveSideAxisName = "Horizontal"; // 좌우 회전을 위한 입력축 이름
     // 값 할당은 내부에서만 가능
     public float move { get; private set; } // 감지된 움직임 입력값
-    public float rotate { get; private set; } // 감지된 회전 입력값
+    public float moveside { get; private set; } // 감지된 회전 입력값
 
 
     // 매프레임 사용자 입력을 감지
     private void Update()
     {
-        
-        rotate = Input.GetAxis(rotateAxisName);
+
+        moveside = Input.GetAxis(moveSideAxisName);
 
         move = Input.GetAxis(moveAxisName);
 
