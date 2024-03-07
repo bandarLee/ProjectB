50a44de02cd60b24047bd5cb70129a1079cb4ec4
yeeejini <rkdska231027@gmail.com>
2024-03-07 15:56:31 +0900

    [민예진] 미션창 구현중

--- Assets/02.Scripts/YeJInTest/NPCTurn.cs
+++ Assets/02.Scripts/YeJInTest/NPCTurn.cs
@@ -14,12 +14,13 @@ public class NPCTurn : MonoBehaviour
     private Animator _animator;
 
     private Transform _targetPlayer;
-    private float FindDistance = 3f;
-
+    private float FindDistance = 5f;
+    private bool _isTurning = false;
     public NPCType _NPCType;
 
-    private UI_Quest QuestUI;
-  
+    public UI_Inventory InventoryUI;
+
+
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -29,13 +30,25 @@ public class NPCTurn : MonoBehaviour
 
     private void Update()
     {
-        if (Vector3.Distance(_targetPlayer.position, transform.position) <= FindDistance)
-        {
-            _animator.SetTrigger("Turn");
-        }
-       
 
-       
+        //Debug.Log(Vector3.Distance(_targetPlayer.position, transform.position));
+        if (Vector3.Distance(_targetPlayer.position, transform.position) < FindDistance)
+        {
+            _isTurning = true;
+            _animator.SetTrigger("Turn");
+            StartCoroutine(Quest_Coroutine());
+        }
+        else if (_isTurning && (Vector3.Distance(_targetPlayer.position, transform.position) > FindDistance))
+        {
+            _animator.SetTrigger("LeftTurn");
+            _isTurning = false;
+        }
+    }
+
+    private IEnumerator Quest_Coroutine() 
+    {
+        yield return new WaitForSeconds(2f);
+        InventoryUI.Open();
     }
 }
 
--- Assets/02.Scripts/YeJInTest/UI_Inventory.cs
+++ Assets/02.Scripts/YeJInTest/UI_Inventory.cs
@@ -1,20 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Inventory : MonoBehaviour
 {
+    public Image ImageUI;
+    public TextMeshProUGUI MissionTextUI;
+    string text;
+
+    private void Start()
+    {
+        
+        MissionTextUI.gameObject.SetActive(false);
+    }
     public void Open() 
     {
-        gameObject.SetActive(true);
+        ImageUI.gameObject.SetActive(true);
+        
+    }
+    public void OpenText() 
+    {
+        MissionTextUI.gameObject.SetActive(true);
+        text = "[플레이어]님!\nZeta-3 행성에서 생명반응이 보였습니다!\n \n첫 번째 임무!\nZeta-3으로 이동하는 포탈을 타고\n생명반응의 원인을 조사해주세요";
+        StartCoroutine(Mission(text));
     }
     public void Close() 
     {
-        gameObject.SetActive(false);
+        ImageUI.gameObject.SetActive(false);
     }
     private void Awake()
     {
-        gameObject.SetActive(false);
+        ImageUI.gameObject.SetActive(false);
     }
+    private IEnumerator Mission(string talk) 
+    {
+        MissionTextUI.text = null;
 
+        for (int i = 0; i < talk.Length; i++) 
+        {
+            MissionTextUI.text += talk[i];
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 }
