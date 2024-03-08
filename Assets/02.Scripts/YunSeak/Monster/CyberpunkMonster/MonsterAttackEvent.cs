cd9e4b28ec6d63968200851b8956f1bf5454b73f
DESKTOP-358CSLP\USER <dbstjr7412369@gmail.com>
2024-03-08 18:17:39 +0900

    [이윤석] 몬스터 제작 중

---
+++ Assets/02.Scripts/YunSeak/Monster/CyberpunkMonster/MonsterAttackEvent.cs
@@ -0,0 +1,17 @@
+using System.Threading;
+using UnityEngine;
+
+public class MonsterAttackEvent : MonoBehaviour
+{
+    private MonsterNormalMove _owner;
+
+    private void Start()
+    {
+        _owner = GetComponentInParent<MonsterNormalMove>();
+    }
+
+    //public void AttackEvent()
+    //{
+    //    _owner.PlayerAttack();
+    //}수정
+}
