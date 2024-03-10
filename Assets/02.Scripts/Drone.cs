685ae785d18de4b112bf86b6a41c8c6b4548aadc
thlee <sheena1131@kakao.com>
2024-03-10 14:56:38 +0900

    [이태환] 드론발사 매커니즘 수정

--- Assets/02.Scripts/Drone.cs
+++ Assets/02.Scripts/Drone.cs
@@ -6,10 +6,16 @@ public class Drone : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float fireRate = 0.5f;
-    private bool isCoolingDown = false; 
+    private bool isCoolingDown = false;
+    public Transform player;
+
+    public Transform fireDronPoint;
+
 
     void Update()
     {
+        this.gameObject.transform.rotation = player.rotation;
+
         if (Input.GetKey(KeyCode.LeftShift) && !isCoolingDown)
         {
             StartCoroutine(ShootWithCooldown());
@@ -18,13 +24,19 @@ public class Drone : MonoBehaviour
 
     IEnumerator ShootWithCooldown()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        this.gameObject.transform.position = fireDronPoint.position;
+
+        Quaternion additionalRotation = Quaternion.Euler(0, -10, 0);
+        Quaternion finalRotation = firePoint.rotation * additionalRotation;
+
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, finalRotation);
         isCoolingDown = true;
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        rb.velocity = transform.forward * 20f;
+        rb.velocity = finalRotation * Vector3.forward * 20f;
 
         yield return new WaitForSeconds(fireRate);
 
         isCoolingDown = false;
     }
+
 }
