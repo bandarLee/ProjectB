553b7e5c067bb0d47585877b2277a30f3300d0a8
yeeejini <rkdska231027@gmail.com>
2024-03-11 12:02:01 +0900

    [민예진]

--- Assets/02.Scripts/GameManager/GameManager.cs
+++ Assets/02.Scripts/GameManager/GameManager.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance { get; private set; }
 
+    public UI_Option OptionUI;
+
+
     private void Awake()
     {
         instance = this;
     }
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
     public void Pause() 
     {
         Time.timeScale = 0f;
@@ -19,5 +28,11 @@ public class GameManager : MonoBehaviour
     public void Continue() 
     {
         Time.timeScale = 1f;
+    }
+
+    public void OnOptionButtonClicked() 
+    {
+        Pause();
+        OptionUI.Open();
     }
 }
---
+++ Assets/02.Scripts/YeJInTest/UI_Option.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Option : MonoBehaviour
+{
+    public void Open() 
+    {
+        gameObject.SetActive(true);
+    }
+    public void Close() 
+    {
+        gameObject.SetActive(false);
+    }
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+    public void OnContinueButtonClicked() 
+    {
+        Close();
+        GameManager.instance.Continue();
+    }
+    public void OnExitButtonClicked() 
+    {
+        
+    }
+}
