04930beb10102d18db2a1b88031d3d66a5b22d6e
thlee <sheena1131@kakao.com>
2024-03-13 17:08:27 +0900

    [이태환] 보스

--- Assets/02.Scripts/Scene1Boss.cs
+++ Assets/02.Scripts/Scene1Boss.cs
@@ -1,16 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scene1Boss : MonoBehaviour
 {
+    public float health = 200;
+    public Transform Cubepoint;
+    public float attackDelay = 0.6f;
+    private float lastAttackTime = 0f;
+    public Slider healthBarSlider;
+    public GameObject healthBarUI;
+    public TextMeshProUGUI damage;
+    GameObject player;
+
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("PlayerHead");
+        healthBarUI.SetActive(false);
+        InitializeHealthBar();
+
     }
 
     void Update()
     {
-        
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        {
+            float damageAmount = 0;
+
+            if (other.gameObject.CompareTag("basicweapon") && Time.time >= lastAttackTime + attackDelay)
+            {
+                healthBarUI.SetActive(true);
+                Debug.Log("기본무기맞음");
+                damageAmount = PlayerStat.instance.str;
+
+                health -= damageAmount;
+                StartCoroutine(ShowDamageCoroutine(damageAmount));
+
+                lastAttackTime = Time.time;
+            }
+            if (other.gameObject.CompareTag("bullet"))
+            {
+                healthBarUI.SetActive(true);
+                PlayerBullet playerbullet = other.gameObject.GetComponent<PlayerBullet>();
+                if (playerbullet.playerbullettype == PlayerBullet.PlayerBulletType.DroneBullet)
+                {
+                    damageAmount = PlayerStat.instance.dronestr;
+                    health -= damageAmount;
+                    StartCoroutine(ShowDamageCoroutine(damageAmount));
+
+
+                }
+                else if (playerbullet.playerbullettype == PlayerBullet.PlayerBulletType.StrongBullet)
+                {
+                    damageAmount = PlayerStat.instance.dronestr * 3;
+                    health -= damageAmount;
+                    StartCoroutine(ShowDamageCoroutine(damageAmount));
+
+                    damage.text = $"{PlayerStat.instance.dronestr * 3}";
+
+                }
+
+            }
+        }
+    }
+    IEnumerator ShowDamageCoroutine(float damageAmount)
+    {
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        if (distance < 30)
+        {
+            damage.fontSize = 36; // 기본 크기
+        }
+
+        else if (distance < 40)
+        {
+            damage.fontSize = 45;
+        }
+        else if (distance < 50)
+        {
+            damage.fontSize = 54;
+        }
+        else if (distance < 60)
+        {
+            damage.fontSize = 63;
+        }
+        else
+        {
+            damage.fontSize = 72;
+        }
+        damage.text = $"{damageAmount}";
+        damage.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+
+        damage.gameObject.SetActive(false);
+    }
+    void InitializeHealthBar()
+    {
+        healthBarSlider.maxValue = health;
+        healthBarSlider.value = health;
     }
 }
