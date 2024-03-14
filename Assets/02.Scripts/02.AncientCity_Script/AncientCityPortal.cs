732428810ad34142f66f813fe9a65ab9fa77abfb
yeeejini <rkdska231027@gmail.com>
2024-03-14 10:04:01 +0900

    [민예진]

--- Assets/02.Scripts/02.AncientCity_Script/AncientCityPortal.cs
+++ Assets/02.Scripts/02.AncientCity_Script/AncientCityPortal.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AncientCityPortal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player")) 
+        {
+            StartCoroutine(NextScene());
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private IEnumerator NextScene()
     {
-        
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("LobbyScene");
     }
 }
